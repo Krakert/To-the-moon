aaba1d881691c1d09705e4de60232dac8b398f73
Stefan de Kraker <stefandekraker1999@gmail.com>
2021-09-27 00:21:17 +0200

    Code is in a state where when no SSID and PSW are present in the EEPROM, setup a softAP, here you can input your wifi's SSID and PSW. Those are then saved to the EEPROM, On powercycle, the data is loaded and connects to the AP you put in

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,24 @@
-#include <ESP8266WiFi.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <SPI.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_ILI9341.h>
+#include <wire.h>
+
 #include <wifi.h>
+#include <eeprom_handler.h>
+
+const char* PARAM_INPUT_1_AP = "ssidFromAp";
+const char* PARAM_INPUT_2_AP = "passwordFromAp";
+
+AsyncWebServer server(WIFI_AP_STANDARD_PORT);
+
+void handleNotFound(AsyncWebServerRequest *request){
+    if (Serial){
+        Serial.println("function: handleNotFound");
+    }
+    request->send(404, "text/plain", "404: Not found");
+}
 
 #define STANDARD_CONFIG_AP   true 
 
@@ -16,46 +35,92 @@
   #define WIFI_AP_PORT  80
 #endif
 
+
 void setup()
 {
   Serial.begin(115200);
   Serial.println();
-
-  // WiFi.begin("pvstsdkjdk","KKCA6TBV8TA7");
-
-  // Serial.print("Connecting");
-  // while (WiFi.status() != WL_CONNECTED)
-  // {
-  //   delay(500);
-  //   Serial.print(".");
-  // }
-  // Serial.println();
-
-  // Serial.print("Connected, IP address: ");
-  // Serial.println(WiFi.localIP());
-
   initEeprom();
-  String ssid = "test";
-  String psw = "tset";
-  saveApToEeprom(ssid, psw);
-  delay(1000);
-  getConfigForEeprom();
-  
-  #if(STANDARD_CONFIG_AP)
-    if(setupSoftApConfig()){
-      if (setupSoftAp()){
-        ESP8266WebServer server(WIFI_AP_STANDARD_PORT);
-      }
+
+  // TODO in setup read EEPROM for SSID and PSW, if empty setup AP
+  // String ssid = "test";
+  // String psw = "tset";
+  // * use 'saveApToEeprom' for 'server.on("/saveApData", handleSaveApData)'
+  // saveApToEeprom(ssid, psw);
+  // TODO make reset fucntion, if GPIO IS high clear eeprom
+  // clearEeprom();
+
+  uint8_t** valuesOutEeprom = getConfigForEeprom();
+  // * Use me to show data of the eeprom `dumbDataEeprom(valuesOutEeprom);`
+  dumbDataEeprom(valuesOutEeprom);
+  if (checkIfNotEmpty(valuesOutEeprom) == 2){
+    if (Serial){
+      Serial.println("Start the STA mode, data valid in the eeprom");
     }
-  #else 
-    if(setupSoftApConfig(LOCAL_IP, GATEWAY, SUBNET)){
-      if (setupSoftAp(WIFI_AP_SSID, WIFI_AP_PSW)){
-        ESP8266WebServer server(WIFI_AP_PORT);
-      }
+    #if(STANDARD_CONFIG_AP)
+      AsyncWebServer server(WIFI_AP_STANDARD_PORT);
+    #else
+      AsyncWebServer server(WIFI_AP_PORT);
+    #endif
+    // ! Start the STA mode, data valid in the eeprom
+    char *ssidFromEeprom = extractParameter(valuesOutEeprom, 0);
+    char *pswFromEeprom = extractParameter(valuesOutEeprom, 1);
+    Serial.println(ssidFromEeprom);
+    Serial.println(pswFromEeprom);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssidFromEeprom, pswFromEeprom);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+      Serial.println("WiFi Failed!");
+      return;
     }
-  #endif
+    Serial.println();
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
+  } else {
+    if (Serial){
+      Serial.println("Setup AP, no data in eeprom");
+    }
+    // ! Setup AP, no data in eeprom
+    #if(STANDARD_CONFIG_AP)
+      
+      setupSoftApConfig();
+      setupSoftAp();   
+    #else
+      AsyncWebServer server(WIFI_AP_PORT); 
+      setupSoftApConfig(LOCAL_IP, GATEWAY, SUBNET);
+      setupSoftAp(WIFI_AP_SSID, WIFI_AP_P SW);
+
+    #endif
+
+    delay(100);
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+      if (Serial){
+        Serial.println("function: serverAp.on(\"/\"");
+    }
+      request->send_P(200, "text/html", INDEX_HTML_AP);
+    });
+
+    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+      if (Serial){
+        Serial.println("function: serverAp.on(\"/get\"");
+      }
+      String ssidToSave, pswToSave;
+      if (request->hasParam(PARAM_INPUT_1_AP)) {
+        ssidToSave = request->getParam(PARAM_INPUT_1_AP)->value();
+      } if (request->hasParam(PARAM_INPUT_2_AP)) {
+        pswToSave = request->getParam(PARAM_INPUT_2_AP)->value();
+      }
+      if (saveApToEeprom(ssidToSave, pswToSave, true) == 0){
+        request->send(200, "text/html", INDEX_HTML_AP_SUCCES);
+      } else {
+        Serial.println("Something went wrong");
+      }
+    });
+
+    server.onNotFound(handleNotFound);
+    server.begin();    
+  }                       
 }
 
 void loop() {
-  
 }
