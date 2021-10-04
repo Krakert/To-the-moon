3a5f6fd326b6ed52689d59b71704f8c9754a2972
Stefan de Kraker <stefandekraker1999@gmail.com>
2021-10-04 14:28:27 +0200

    Init file for handeling the LCD screen

--- lib/displayHandler/displayHandler.cpp
+++ lib/displayHandler/displayHandler.cpp
@@ -1,10 +1,115 @@
 #include <displayHandler.h>
 
-displayHandler::displayHandler(uint8_t tftCs, uint8_t tftDc, uint8_t tftRst, uint8_t tsCs, uint8_t displayRotation, uint8_t touchRotation){
-    tft = Adafruit_ILI9341(tftCs, tftDc, tftRst);
-    XPT2046_Touchscreen ts(TS_CS);
+/*
+<---X axis--->
+---------------
+|             |  ^
+|             |  | Y axis
+|             |  
+---------------
+*/
+
+/**************************************************************************/
+/*!
+  @brief    Set up the TFT display
+  @param    touchRotation           uint8_t:  The rotation  of the touchscreen (x,Y position)
+  @param    screenRotation          uint8_t:  The rotation  of the  text on the display (x,Y position)
+  @param    textSize                uint8_t:  The default text size
+  @param    textColor               uint16_t: The default color of the text
+*/
+/**************************************************************************/
+void displayHandler::init(uint8_t touchRotation, uint8_t screenRotation, uint8_t textSize, uint16_t textColor){
+    tft.begin();
     ts.begin();
     ts.setRotation(touchRotation);
-    tft.begin();
-    tft.setRotation(displayRotation);
+    tft.setRotation(screenRotation);
+    tft.fillScreen(ILI9341_BLACK);
+    tft.setTextSize(textSize);
+    tft.setTextColor(textColor);
+    displayHandler::textColor = textColor;
+    displayHandler::textSize = textSize;
 }
+
+/**************************************************************************/
+/*!
+  @brief    Show the init screen when setting the AP (first setup)
+*/
+/**************************************************************************/
+void displayHandler::showInitScreenAp(){
+  tft.fillScreen(ILI9341_WHITE);
+  tft.drawRect(X_OFFSET, Y_OFFSET, WIDTH, HEIGHT, ILI9341_BLACK);
+  uint16_t i = 10;
+  for (i = 10; i < 290; i = i + 10){
+    tft.fillRect(X_OFFSET+1, Y_OFFSET+1, i-2, HEIGHT-2, ILI9341_GREEN);
+    delay(10);
+  }
+  tft.fillRect(X_OFFSET, Y_OFFSET, i, HEIGHT, ILI9341_WHITE);
+  placeTextInCenter("Setup is done", (DISPLAY_X_MAX / 2), Y_OFFSET, 3, ILI9341_BLUE);
+  tft.drawLine(10, (Y_OFFSET + (HEIGHT / 2)), (DISPLAY_X_MAX - 10),(Y_OFFSET + (HEIGHT / 2)), ILI9341_BLACK);
+}
+
+/**************************************************************************/
+/*!
+  @brief    Place text in the center of a X an Y coordinate
+  @param    textBuf                 const String: pointer to a String
+  @param    x                       uint16_t:     The X coordinate
+  @param    y                       uint16_t:     The Y coordinate
+  @param    textSize                uint8_t:      The text size
+  @param    textColor               uint16_t:     The color of the text
+  @return   size                    uint16_t[0]:  The width of the text
+                                    uint16_t[1]:  The height of the text
+*/
+/**************************************************************************/
+uint16_t * displayHandler::placeTextInCenter(const String &textBuf, uint16_t x, uint16_t y, uint8_t textSize, uint16_t textColor){
+    int16_t x1, y1;
+    static uint16_t size[2] = {0, 0};
+    tft.setTextSize(textSize);
+    tft.setTextColor(textColor);
+    tft.getTextBounds(textBuf, 0, 0, &x1, &y1, &size[0], &size[1]); //calc width of new string
+    tft.setCursor(x - size[0] / 2, y - size[1] / 2);
+    tft.print(textBuf);
+    return size;
+}
+
+/**************************************************************************/
+/*!
+  @brief    Place a rectangular in the center of a X an Y coordinate
+  @param    x                       uint16_t: X coordinate of the center of the rectangle
+  @param    Y                       uint16_t: Y coordinate of the center of the rectangle
+  @param    w                       uint16_t: Width of the rectangle
+  @param    h                       uint16_t: Height of the rectangle
+  @param    color                   uint16_t: Color of the rectangle
+*/
+/**************************************************************************/
+void displayHandler::placeBoxtInCenter(uint16_t x,  uint16_t y, uint16_t w, uint16_t h, uint16_t color){
+    tft.drawRect((x - (w / 2)), (y - (h / 2)), w, h , color);
+}
+
+/**************************************************************************/
+/*!
+  @brief    Restore text size to default state
+*/
+/**************************************************************************/
+void displayHandler::setTextSizeToDefault(){
+    tft.setTextSize(displayHandler::textSize);
+}
+
+/**************************************************************************/
+/*!
+  @brief    Restore text color to default state
+*/
+/**************************************************************************/
+void displayHandler::setTextColorToDefault(){
+    tft.setTextColor(displayHandler::textColor);
+}
+
+/**************************************************************************/
+/*!
+  @brief    Restore both text size and text color to default state
+*/
+/**************************************************************************/
+void displayHandler::setToDefault(){
+    tft.setTextSize(displayHandler::textSize);
+    tft.setTextColor(displayHandler::textColor);
+}
+
--- lib/displayHandler/displayHandler.h
+++ lib/displayHandler/displayHandler.h
@@ -15,18 +15,54 @@
 #define DISPLAY_ROTATION    3
 #define TOUCH_ROTATION      1
 
+/* DEFINE MAX SCREEN SIZE */
+#define DISPLAY_X_MAX       320 
+#define DISPLAY_Y_MAX       240 
+
+/* SETUP STANDAARD COLOR AND SIZE */
+#define STANDARD_TEXT_SIZE  2
+#define STANDARD_TEXT_COLOR ILI9341_WHITE
+
+/* DEFINE SIZE OF THE PROGRESS BAR */
+#define X_OFFSET            20
+#define Y_OFFSET            40
+#define WIDTH               280
+#define HEIGHT              50
+#define BAR_CENTER_X        X_OFFSET + (WIDTH / 2)
+#define BAR_CENTER_Y        Y_OFFSET + (HEIGHT / 2)
+
+
 class displayHandler {
+    Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_RST);
+    XPT2046_Touchscreen ts = XPT2046_Touchscreen(TS_CS);
     public:
-        displayHandler(uint8_t tftCs = TFT_CS, 
-                       uint8_t tftDc = TFT_DC, 
-                       uint8_t tftRst = TFT_RST, 
-                       uint8_t tsCs = TS_CS,
-                       uint8_t displayRotation = DISPLAY_ROTATION,
-                       uint8_t touchRotation = TOUCH_ROTATION);
-        void showInitScreen();
-    private:    
-        Adafruit_ILI9341 tft;
-        XPT2046_Touchscreen ts;
+        void showInitScreenAp(); 
+
+        const uint16_t displayXSize = DISPLAY_X_MAX;
+        const uint16_t displayYSize = DISPLAY_Y_MAX;
+        void init(uint8_t touchRotation = TOUCH_ROTATION, 
+                  uint8_t screenRotation = DISPLAY_ROTATION, 
+                  uint8_t textSize = STANDARD_TEXT_SIZE, 
+                  uint16_t textColor = STANDARD_TEXT_COLOR);
+
+        uint16_t * placeTextInCenter(const String &textBuf, 
+                                     uint16_t x, 
+                                     uint16_t y, 
+                                     uint8_t textSize = STANDARD_TEXT_SIZE, 
+                                     uint16_t textColor = STANDARD_TEXT_COLOR);
+
+        void placeBoxtInCenter(uint16_t x, 
+                               uint16_t y, 
+                               uint16_t w, 
+                               uint16_t h,
+                               uint16_t color = STANDARD_TEXT_COLOR);
+
+    private:
+        uint8_t textSize = STANDARD_TEXT_SIZE;
+        uint16_t textColor = STANDARD_TEXT_COLOR;
+        void setTextSizeToDefault();
+        void setTextColorToDefault();
+        void setToDefault();
 };
 
 #endif // DISPLAYHANDLER_H
