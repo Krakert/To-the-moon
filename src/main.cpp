cd7e6c5a2cc69ea57945774faa9e583aee96be7b
Stefan de Kraker <stefandekraker1999@gmail.com>
2021-10-05 14:24:41 +0200

    Simple program to show Boot and Init screen AP

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include <displayHandler.h>
+#include <wifi.h>
+displayHandler display;
+
 void setup(){
+  Serial.begin(115200);
+  display.init(1,3);
+  display.boot();
+  display.showInitScreenAp();
 }
 
 void loop(){
+
 }
