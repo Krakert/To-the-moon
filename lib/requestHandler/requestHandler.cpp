604d50a66b6c89349bd694a4bffddb5bd60fa5bb
Stefan de Kraker <stefandekraker1999@gmail.com>
2021-10-30 17:08:25 +0200

    resolved #10, added HTTP GET call to database

--- lib/requestHandler/requestHandler.cpp
+++ lib/requestHandler/requestHandler.cpp
@@ -3,18 +3,18 @@
 /**************************************************************************/
 /*!
   @brief    Asks the API for the data of a coin and place it in a Struct
-  @param    coinId                  String: SSID name, max 32 bytes
-  @param    namePriceStruct         Struct*: pointer to a Struct with 2 Strings
+  @param    coinId                      String: SSID name, max 32 bytes
+  @param    namePriceTimeStruct         Struct*: pointer to a Struct with 3 Strings
 */
 /**************************************************************************/
-void requestHandler::requestFromApi(String coinId, namePriceStruct *namePriceStruct, uint8_t debug){
-    
+void requestHandler::requestFromApi(String coinId, namePriceTimeStruct *namePriceTimeStruct, uint8_t debug){
     if (Serial){
         Serial.println("Class requestHandler, function: requestFromApi");
     }
     String completPath = REQUEST_ADDRESS_API + coinId;
     String payload;
-    http.begin(wifiClient, completPath); 
+    HTTPClient http;
+    http.begin(clientHttpCall, completPath); 
     int httpCode = http.GET(); 
     if (httpCode > 0) { 
       payload = http.getString();
@@ -28,6 +28,7 @@ void requestHandler::requestFromApi(String coinId, namePriceStruct *namePriceStr
     JsonObject filter_data = filter.createNestedObject("data");
     filter_data["id"] = true;
     filter_data["price"] = true;
+    filter_data["price_timestamp"] = true;
 
     DeserializationError error = deserializeJson(doc, payload, DeserializationOption::Filter(filter));
 
@@ -37,7 +38,106 @@ void requestHandler::requestFromApi(String coinId, namePriceStruct *namePriceStr
             Serial.println(error.f_str());
         }
     } else{
-    namePriceStruct->idCoin = doc["data"]["id"].as<String>();
-    namePriceStruct->priceCoin =  doc["data"]["price"].as<String>();
+        namePriceTimeStruct->idCoin = doc["data"]["id"].as<String>();
+        namePriceTimeStruct->priceCoin =  doc["data"]["price"].as<String>();
+        String correctDate = doc["data"]["price_timestamp"].as<String>();
+        correctDate.replace("Z", "");
+        namePriceTimeStruct->timeStamp = correctDate;
     }
+}
+
+/**************************************************************************/
+/*!
+  @brief    Insert data of a coin of the API into the Database
+  @param    namePriceTimeStruct         Struct*: Pointer to a Struct with 3 Strings
+  @param    debug                       uint8_t: True, false, when true print more data of the call
+  @return   0 / 1 / 2                   uint8_t: 0, Call unsuccessful
+                                                 1, Call successful
+                                                 2, Error (404)
+*/
+/**************************************************************************/
+uint8_t requestHandler::requestInsertDataCoin(namePriceTimeStruct *namePriceTimeStruct, uint8_t debug){
+    if (Serial){
+        Serial.println("Class requestHandler, function: requestToDatabase");
+    }
+    String payload;
+    sprintf(fullHttpCall, "%s%d%s/%s/%s/%s", REQUEST_ADDRESS_DATABASE_BASE, DATABASE_PORT_NUMBER, REQUEST_DATABASE_INSERT_DATA,
+                                             namePriceTimeStruct->idCoin.c_str(), 
+                                             namePriceTimeStruct->timeStamp.c_str(), 
+                                             namePriceTimeStruct->priceCoin.c_str());
+    if (debug){
+        char buffer[256];
+        sprintf(buffer, "doing call to URL: %s", fullHttpCall);
+        Serial.println(buffer);
+    }
+    HTTPClient http;
+    http.begin(clientHttpCall, fullHttpCall); 
+    int httpCode = http.GET(); 
+    if (httpCode > 0) { 
+      payload = http.getString();
+      if (Serial && debug){
+          Serial.println(payload); 
+      }
+    }
+    http.end();
+    if (payload == "true"){
+        return 1;
+    } else if (payload == "false"){
+        return 0;
+    } else {
+        return 2;
+    }
+}
+/**************************************************************************/
+/*!
+  @brief    Asks the API for all the coins thats needs to be tracked
+  @param    debug                       uint8_t: True, false, when true print more data of the call
+  @return   arrayCoins                  uint8_t[32]: Array of the data from the call in uint8_t
+*/
+/**************************************************************************/
+uint8_t * requestHandler::requestGetCoinsDatabase(uint8_t debug){
+    if (Serial){
+        Serial.println("Class requestHandler, function: requestToDatabase");
+    }
+    String payload;
+    sprintf(fullHttpCall, "%s%d%s", REQUEST_ADDRESS_DATABASE_BASE, 
+                                     DATABASE_PORT_NUMBER, 
+                                     REQUEST_DATABASE_LIST_COINS);
+    if (Serial && debug){
+        char buffer[256];
+        sprintf(buffer, "doing call to URL: %s", fullHttpCall);
+        Serial.println(buffer);
+    }
+    HTTPClient http;
+    http.begin(clientHttpCall, fullHttpCall); 
+    int httpCode = http.GET(); 
+    if (httpCode > 0) { 
+      payload = http.getString();
+      if (Serial && debug){
+          Serial.println(payload); 
+      }
+    }
+    DeserializationError error = deserializeJson(doc, payload);
+
+    if (error) {
+        if (Serial){
+            Serial.print(F("deserializeJson() failed: "));
+            Serial.println(error.f_str());
+        }
+    }
+    static uint8_t arrayCoins[BYTES_NEEDED_FROM_EEPROM_COINS];
+    uint8_t index = 0;
+    for (JsonObject item : doc.as<JsonArray>()) {        
+        String coin = item["coin_id"].as<String>();
+        for (uint8_t i  = 0; i < coin.length(); i++){
+            if (Serial && debug){
+                Serial.println(coin[i]);
+                Serial.print(arrayCoins[index + i]);    
+                Serial.println();   
+            }
+            arrayCoins[index + i] = (uint8_t)coin[i];           
+        }
+        index = index + BYTES_PER_ID_COIN;
+    }
+    return arrayCoins;
 }
--- lib/requestHandler/requestHandler.h
+++ lib/requestHandler/requestHandler.h
@@ -3,24 +3,31 @@
 
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
+#include <eepromHandler.h>
 
-#define REQUEST_ADDRESS_API     "http://api.iot.hva-robots.nl/crypto/"
+#define REQUEST_ADDRESS_API                 "http://api.iot.hva-robots.nl/crypto/"
+#define DATABASE_PORT_NUMBER                8112
+#define REQUEST_ADDRESS_DATABASE_BASE       "http://oege.ie.hva.nl:"
+#define REQUEST_DATABASE_INSERT_DATA        "/insert/data"
+#define REQUEST_DATABASE_LIST_COINS         "/get/coins"
 
 typedef struct {
     String idCoin;
     String priceCoin;
-} namePriceStruct;
+    String timeStamp;
+} namePriceTimeStruct;
 
 
 class requestHandler{
     
     public:
-        void requestFromApi(String coinId, namePriceStruct *namePriceStruct, uint8_t debug);
-
+        void requestFromApi(String coinId, namePriceTimeStruct *namePriceTimeStruct, uint8_t debug);
+        uint8_t requestInsertDataCoin(namePriceTimeStruct *namePriceTimeStruct, uint8_t debug);
+        uint8_t * requestGetCoinsDatabase(uint8_t debug);
     private:
-        WiFiClient wifiClient;    
-        HTTPClient http; 
-        StaticJsonDocument<48> filter;
-        StaticJsonDocument<128> doc;
+        WiFiClient clientHttpCall;    
+        StaticJsonDocument<64> filter;
+        StaticJsonDocument<512> doc;
+        char fullHttpCall[128];
 };
 #endif // REQUESTHANDLER_H
